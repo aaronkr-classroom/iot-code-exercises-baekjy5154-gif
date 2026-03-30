17fedc30445b4d537fdc7beabec0f32aff850d9d
baekjy5154-gif <baekjy5154@gmail.com>
2026-03-30 09:44:38 +0900

    Add files via upload

---
+++ code/3장/3_set.py
@@ -0,0 +1,21 @@
+# 3_set.py
+# 두 집합 정의
+set1 = {1,2,3,'a',"Hello"}
+set2 = {"Hello",3,4,5,'b'}
+
+# 합집합
+union_set = set | set2
+
+# 교집합
+int_set = set1 & set2
+
+# 차집합
+diff_set = set1 - set2
+
+# 대칭 차집합
+sym_diff_set = set1 ^ set2
+
+print('union:', union_set)
+print(f"intersection: {int_set}")
+print(f"difference: {diff_set}")
+print(f"symmetric difference: {sym_diff_set}")
